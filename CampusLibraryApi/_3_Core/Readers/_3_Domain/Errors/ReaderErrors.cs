db531c0def219573a593d18cfc5927fe75bb4449
Bernd-Uwe Rogalla <bu.rogalla@icloud.com>
2026-06-07 15:22:52 +0200

    Fix reader errors naming and statuses

--- CampusLibraryApi/_3_Core/Readers/_3_Domain/Errors/ReaderErrors.cs
+++ CampusLibraryApi/_3_Core/Readers/_3_Domain/Errors/ReaderErrors.cs
@@ -1,106 +1,142 @@
-using System.Runtime.InteropServices.JavaScript;
 using CampusLibraryApi._2_Shared._3_Domain.Enums;
 using CampusLibraryApi._2_Shared._3_Domain.Errors;
+
 namespace CampusLibraryApi._3_Core.Readers._3_Domain.Errors;
 
 public static class ReaderErrors {
-   
+
    public static readonly DomainError IdRequired =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Id is Required", 
-         "Reader id is required.");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: IdRequired",
+         "Reader id is required."
+      );
 
    public static readonly DomainError InvalidId =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Invalid Id",
-         "The given Id is invalid.");
-   
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: InvalidId",
+         "The given Id is invalid."
+      );
+
    public static readonly DomainError SubjectRequired =
-      new(WebErrorStatus.BadRequest,
-         "Reader.SubjectRequired", 
-         "Subject is required.");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: SubjectRequired",
+         "Subject is required."
+      );
 
    public static readonly DomainError FirstnameIsRequired =
-      new(WebErrorStatus.BadRequest,
-         "Reader: First name required",
-         "A first name must be provided.");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: FirstnameRequired",
+         "A first name must be provided."
+      );
 
    public static readonly DomainError InvalidFirstname =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Invalid first name", 
-         "The provided first name is too short or too long (2–80 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: InvalidFirstname",
+         "The provided first name is too short or too long (2–80 characters)."
+      );
 
    public static readonly DomainError LastnameIsRequired =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Last name required",
-         "A last name must be provided.");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: LastnameRequired",
+         "A last name must be provided."
+      );
 
    public static readonly DomainError InvalidLastname =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Invalid last name",
-         "The provided last name is too short or too long (2–80 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: InvalidLastname",
+         "The provided last name is too short or too long (2–80 characters)."
+      );
 
    public static readonly DomainError InvalidEmail =
-      new(WebErrorStatus.BadRequest,
-         "Reader: Email is invalid", 
-         "The email is invalid.");
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: InvalidEmail",
+         "The email is invalid."
+      );
 
    public static readonly DomainError EmailAlreadyInUse =
-      new(WebErrorStatus.BadRequest,
-         "Email: Email already used",
-         "The provided email address is already in use.");
-   
+      new(
+         WebErrorStatus.Conflict,
+         "Reader: EmailAlreadyInUse",
+         "The provided email address is already in use."
+      );
+
    public static readonly DomainError ReaderNotFound =
-      new(WebErrorStatus.NotFound,
-         "Reader: Not Found", 
-         "The reader was not found.");
+      new(
+         WebErrorStatus.NotFound,
+         "Reader: NotFound",
+         "The reader was not found."
+      );
 
    public static readonly DomainError SubjectAlreadyExists =
-      new(WebErrorStatus.Conflict,
-         "Reader: Subject already exists",
-         "A reader with this subject already exists.");
-   
-   public static readonly DomainError CustomerCreateDtoRequired =
-      new(WebErrorStatus.BadRequest,
-         "Reader: ReaderCreateDto required",
-         "A ReaderCreateDto object must must be provided.");
+      new(
+         WebErrorStatus.Conflict,
+         "Reader: SubjectAlreadyExists",
+         "A reader with this subject already exists."
+      );
 
-   
-   // Address (Value Object)
+   public static readonly DomainError ReaderCreateDtoRequired =
+      new(
+         WebErrorStatus.BadRequest,
+         "Reader: ReaderCreateDtoRequired",
+         "A ReaderCreateDto object must be provided."
+      );
+
+   // Address value object
    // ------------------------------------------------------------------------
    public static readonly DomainError StreetIsRequired =
-      new(WebErrorStatus.BadRequest,
-         "Address: Street is required",
-         "A street must be provided when specifying an address.");
-   
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: StreetRequired",
+         "A street must be provided when specifying an address."
+      );
+
    public static readonly DomainError InvalidStreet =
-      new(WebErrorStatus.BadRequest,
-         "Address: Invalid street name",
-         "The provided street name is too short or too long (2–80 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: InvalidStreet",
+         "The provided street name is too short or too long (2–80 characters)."
+      );
 
    public static readonly DomainError PostalCodeIsRequired =
-      new(WebErrorStatus.BadRequest,
-         "Address: Postal code is required",
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: PostalCodeRequired",
          "A postal code must be provided when specifying an address."
       );
+
    public static readonly DomainError InvalidPostalCode =
-      new(WebErrorStatus.BadRequest,
-         "Address: Invalid postal code",
-         "The provided postal code is too short or too long (2–10 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: InvalidPostalCode",
+         "The provided postal code is too short or too long (2–10 characters)."
+      );
 
    public static readonly DomainError CityIsRequired =
       new(
          WebErrorStatus.BadRequest,
-         "Address:City is required",
+         "Address: CityRequired",
          "A city must be provided when specifying an address."
       );
+
    public static readonly DomainError InvalidCity =
-      new(WebErrorStatus.BadRequest,
-         "Address: Invalid city",
-         "The provided city is too short or too long (2–80 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: InvalidCity",
+         "The provided city is too short or too long (2–80 characters)."
+      );
 
    public static readonly DomainError InvalidCountry =
-      new(WebErrorStatus.BadRequest,
-         "Address: Invalid country",
-         "The provided country is too short or too long (2–80 characters).");
+      new(
+         WebErrorStatus.BadRequest,
+         "Address: InvalidCountry",
+         "The provided country is too short or too long (2–80 characters)."
+      );
 }
