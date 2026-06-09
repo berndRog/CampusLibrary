d1dd0884cda331f138f9c6e084db72e41b7d3afa
Bernd-Uwe Rogalla <bu.rogalla@icloud.com>
2026-06-09 19:15:23 +0200

    Add reader controller end-to-end tests

---
+++ CampusLibraryApiTest/_4_ApiTests/ReadersControllerE2eT.cs
@@ -0,0 +1,199 @@
+using System.Net;
+using System.Net.Http.Json;
+using AwesomeAssertions;
+using CampusLibraryApi._2_Shared._1_Ports;
+using CampusLibraryApi._3_Core.Readers._1_Ports;
+using CampusLibraryApi._3_Core.Readers._2_Application.Dtos;
+using CampusLibraryApi._3_Core.Readers._2_Application.Mappings;
+using CampusLibraryApiTest.TestController;
+using CampusLibraryApiTest.TestHelper.Mappings;
+using CampusLibraryApiTest.TestInfrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CampusLibraryApiTest._4_ApiTests;
+
+public sealed class ReadersControllerE2eT : TestBaseEndToEnd {
+   protected override string DatabaseName => nameof(ReadersControllerE2eT);
+
+   [Fact]
+   public async Task GetByIdAsync_ok() {
+      // Arrange
+      ReaderDto expectedReaderDto = default!;
+
+      await Factory.WithScopeAsync(async sp => {
+         var repository = sp.GetRequiredService<IReaderRepository>();
+         var unitOfWork = sp.GetRequiredService<IUnitOfWork>();
+         var seed = sp.GetRequiredService<TestSeed>();
+
+         var reader = seed.Reader1();
+         expectedReaderDto = reader.ToReaderDto();
+
+         repository.Add(reader);
+         await unitOfWork.SaveAllChangesAsync("Reader1 inserted", TestContext.Current.CancellationToken);
+         unitOfWork.ClearChangeTracker();
+      });
+
+      // Act
+      var response = await Client.GetAsync(
+         $"/library/v1/readers/{expectedReaderDto.Id}",
+         TestContext.Current.CancellationToken
+      );
+      var actualReaderDto = await response.Content.ReadFromJsonAsync<ReaderDto>(
+         cancellationToken: TestContext.Current.CancellationToken
+      );
+
+      // Assert
+      response.StatusCode.Should().Be(HttpStatusCode.OK);
+      actualReaderDto.Should().NotBeNull();
+      actualReaderDto.Should().BeEquivalentTo(expectedReaderDto);
+   }
+
+   [Fact]
+   public async Task GetAllAsync_ok() {
+      // Arrange
+      List<ReaderDto> expectedReaderDtos = [];
+
+      await Factory.WithScopeAsync(async sp => {
+         var repository = sp.GetRequiredService<IReaderRepository>();
+         var unitOfWork = sp.GetRequiredService<IUnitOfWork>();
+         var seed = sp.GetRequiredService<TestSeed>();
+
+         var readers = seed.Readers;
+         expectedReaderDtos = readers
+            .Select(r => r.ToReaderDto())
+            .OrderBy(r => r.Id)
+            .ToList();
+
+         repository.AddRange(readers);
+         await unitOfWork.SaveAllChangesAsync("Readers inserted", TestContext.Current.CancellationToken);
+         unitOfWork.ClearChangeTracker();
+      });
+
+      // Act
+      var response = await Client.GetAsync(
+         "/library/v1/readers",
+         TestContext.Current.CancellationToken
+      );
+      var actualReaderDtos = await response.Content.ReadFromJsonAsync<List<ReaderDto>>(
+         cancellationToken: TestContext.Current.CancellationToken
+      );
+
+      // Assert
+      response.StatusCode.Should().Be(HttpStatusCode.OK);
+      actualReaderDtos.Should().NotBeNull();
+      actualReaderDtos!
+         .OrderBy(r => r.Id)
+         .Should()
+         .BeEquivalentTo(expectedReaderDtos);
+   }
+
+   [Fact]
+   public async Task CreateAsync_ok() {
+      // Arrange
+      ReaderCreateDto dto = default!;
+
+      await Factory.WithScopeAsync(sp => {
+         var seed = sp.GetRequiredService<TestSeed>();
+         dto = Mappings.ToReaderCreateDto(seed.ReaderRegister());
+         return Task.CompletedTask;
+      });
+
+      // Act
+      var response = await Client.PostAsJsonAsync(
+         "/library/v1/readers",
+         dto,
+         TestContext.Current.CancellationToken
+      );
+      var actualReaderDto = await response.Content.ReadFromJsonAsync<ReaderDto>(
+         cancellationToken: TestContext.Current.CancellationToken
+      );
+
+      // Assert
+      response.StatusCode.Should().Be(HttpStatusCode.Created);
+      response.Headers.Location.Should().NotBeNull();
+      actualReaderDto.Should().NotBeNull();
+      actualReaderDto!.Id.Should().Be(Guid.Parse(dto.Id!));
+      actualReaderDto.Firstname.Should().Be(dto.Firstname);
+      actualReaderDto.Lastname.Should().Be(dto.Lastname);
+      actualReaderDto.Email.Should().Be(dto.Email);
+      actualReaderDto.Subject.Should().Be(dto.Subject);
+   }
+
+   [Fact]
+   public async Task UpdateAsync_ok() {
+      // Arrange
+      ReaderDto expectedReaderDto = default!;
+      ReaderUpdateDto updateDto = default!;
+
+      await Factory.WithScopeAsync(async sp => {
+         var repository = sp.GetRequiredService<IReaderRepository>();
+         var unitOfWork = sp.GetRequiredService<IUnitOfWork>();
+         var seed = sp.GetRequiredService<TestSeed>();
+
+         var reader1 = seed.Reader1();
+         var reader2 = seed.Reader2();
+         updateDto = Mappings.ToReaderUpdateDto(reader2);
+
+         expectedReaderDto = new ReaderDto(
+            Id: reader1.Id,
+            Subject: reader1.Subject,
+            Firstname: updateDto.Firstname,
+            Lastname: updateDto.Lastname,
+            Email: updateDto.Email,
+            AddressDto: updateDto.AddressDto
+         );
+
+         repository.Add(reader1);
+         await unitOfWork.SaveAllChangesAsync("Reader1 inserted", TestContext.Current.CancellationToken);
+         unitOfWork.ClearChangeTracker();
+      });
+
+      // Act
+      var response = await Client.PutAsJsonAsync(
+         $"/library/v1/readers/{expectedReaderDto.Id}",
+         updateDto,
+         TestContext.Current.CancellationToken
+      );
+      var actualReaderDto = await response.Content.ReadFromJsonAsync<ReaderDto>(
+         cancellationToken: TestContext.Current.CancellationToken
+      );
+
+      // Assert
+      response.StatusCode.Should().Be(HttpStatusCode.OK);
+      actualReaderDto.Should().NotBeNull();
+      actualReaderDto.Should().BeEquivalentTo(expectedReaderDto);
+   }
+
+   [Fact]
+   public async Task DeleteAsync_ok() {
+      // Arrange
+      Guid readerId = default;
+
+      await Factory.WithScopeAsync(async sp => {
+         var repository = sp.GetRequiredService<IReaderRepository>();
+         var unitOfWork = sp.GetRequiredService<IUnitOfWork>();
+         var seed = sp.GetRequiredService<TestSeed>();
+
+         var reader = seed.Reader3();
+         readerId = reader.Id;
+
+         repository.Add(reader);
+         await unitOfWork.SaveAllChangesAsync("Reader3 inserted", TestContext.Current.CancellationToken);
+         unitOfWork.ClearChangeTracker();
+      });
+
+      // Act
+      var deleteResponse = await Client.DeleteAsync(
+         $"/library/v1/readers/{readerId}",
+         TestContext.Current.CancellationToken
+      );
+      var getResponse = await Client.GetAsync(
+         $"/library/v1/readers/{readerId}",
+         TestContext.Current.CancellationToken
+      );
+
+      // Assert
+      deleteResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+      getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+   }
+}
